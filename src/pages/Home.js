121abfb8328cc5c62b43bf471cea27e287ffa5f4
Thai Son Nguyen <thaison_nguyen@discovery.com>
2020-01-08 18:23:28 +0800

    binding data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import { AfnContext } from "../Context";
 import Header from "../components/Header"
 import Container from "../components/Container"
 import Footer from "../components/Footer"
 
 export default class Home extends Component {
-    
+    static contextType = AfnContext;
     render() {
-        const { location, children } = this.props
+        let recipe = this.context;
+        console.log(recipe.title)
         return (
             <div>
                 <Header />
                 <Container>
-                    <span>asdasdasdasdas</span>
+                    <span>{recipe.title}</span>
                 </Container>
                 <Footer />
             </div>
